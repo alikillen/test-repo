e9a0b1da2d9adafdb575525078d719adae46bdba
alikillen <alisoneme1994@gmail.com>
2021-03-26 14:30:34 +1000

    testing changes from POPOS

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function double (num) {
   return num*2
 }
 
-console.log(double(2))
+console.log(double(3))
 
 console.log("will I be able to push this to github?")
 
@@ -13,3 +13,5 @@ console.log("pushing from new dell laptop")
 console.log("pushing after ubuntu reset")
 
 console.log("pushing after ssh permissions 700 from 755")
+
+console.log("pushing from new POP OS installed on SSD march 2021")
